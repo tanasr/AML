70dd69c7f3aea5659e888cf72604f868a63f03a2
eldorado <tanasic.r@outlook.com>
2021-10-25 23:17:05 +0200

    tidy up and comment

--- task0/task0.py
+++ task0/task0.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import scipy as sp
 import sklearn as sk
-# from sklearn import datasets
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
@@ -13,37 +12,37 @@ print(squareroot)
 
 iris = datasets.load_iris()
 """
-train = pd.read_csv("train.csv")
-#print(np.mean(train.iloc[0][2:12]))
 
+#load data sets
+data_train = pd.read_csv("train.csv")
+data_test = pd.read_csv("test.csv")
+#print(np.mean(train.iloc[0][2:12]))
 #X = train.iloc[0][2:12]
 
-x_values = train.drop(['Id','y'],axis=1)
-y_value = train['y']
+#get x and y values for training
+x_train = data_train.drop(['Id','y'],axis=1)
+y_train = data_train['y']
+
+# create LinearRegression model
 LR = LinearRegression()
-LR.fit(x_values,y_value)
+LR.fit(x_train,y_train)
 
-testing = pd.read_csv("test.csv")
-x_test = testing.drop('Id',axis=1)
-# print(x_test)
 
-# print(testing)
-y_id = testing['Id']
+# get observations to test the model
+x_test = data_test.drop('Id',axis=1)
+
+# predict y values with knowledge from training
+y_id = data_test['Id']
 y_prediction =  LR.predict(x_test)
+
+#create results which will be submitted 
 y_prediction = pd.DataFrame(y_prediction,columns=['y'])
-# print(type(y_prediction))
-# submission = pd.DataFrame(y_prediction)
 submission = pd.concat([y_id,y_prediction],axis=1)
 
+#create metric to test the model's accuracy
 y_test = np.mean(np.transpose(x_test))
-# print(y_test)
-
 RMSE = mean_squared_error(y_test, y_prediction)**0.5
-print(RMSE)
+print('the error is: ' + str(RMSE))
 # submission.to_csv('out.csv',index=False)
-# print(type(submission))
 
-
-
-# print(submission)
 
