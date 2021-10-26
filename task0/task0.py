e31c4b9c95aae5b92e878e6aeda68aa0415b96f0
eldorado <tanasic.r@outlook.com>
2021-10-26 11:14:53 +0200

    tidy up

--- task0/task0.py
+++ task0/task0.py
@@ -1,10 +1,12 @@
+"""
+AML task0
+Linear Regression problem
+"""
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
-"""
-task0 - Linear Regression problem
-"""
+
 
 #load data sets
 data_train = pd.read_csv("train.csv")
