84ff9fb9116b7ca587578ec98d9b2cf75ae1e482
eldorado <tanasic.r@outlook.com>
2021-10-27 09:55:37 +0200

    test

--- task0/task0.py
+++ task0/task0.py
@@ -8,6 +8,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
 
+
 #load data sets
 data_train = pd.read_csv("train.csv")
 data_test = pd.read_csv("test.csv")
